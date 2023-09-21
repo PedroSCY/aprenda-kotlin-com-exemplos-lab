4760b4eec9b80c6ca47170ca68c6e16c07036d8f
Pedro Lucas G. Prata <78231145+PedroSCY@users.noreply.github.com>
2023-09-21 15:37:39 -0300

    Base

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,61 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, val idade: Int, val email: String) {
+    override fun equals(other: Any?) = other is Usuario && other.email == this.email    
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val id: Int, var nome: String, val nivel: Nivel, val duracao: Int = 60) {
+    override fun equals(other: Any?) = other is ConteudoEducacional && other.id == this.id  
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: MutableSet<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableSetOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario);
+    }
+    
+    fun cancelarMatricula(email: String){
+		val user = inscritos.filter {it.email.equals(email)}[0];
+        inscritos.remove(user);
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    val user1 = Usuario("Pedro", 17, "user1@gmail.com")
+    val user2 = Usuario("Lucas", 22, "user2@gmail.com")
+    val user3 = Usuario("Joao", 20, "user1@gmail.com")
+    
+    val CE1 = ConteudoEducacional(1, "Hello Kotlin", Nivel.BASICO ,30)
+    val CE2 = ConteudoEducacional(2, "Hello JAVA", Nivel.INTERMEDIARIO)
+    val CE3 = ConteudoEducacional(3, "Hello JAVA", Nivel.INTERMEDIARIO,20)
+    val CE4 = ConteudoEducacional(4, "Hello Python", Nivel.AVANCADO, 90)
+    
+    var FJK = Formacao("Formacao Java-Kotlin", mutableSetOf(CE1, CE2, CE3, CE4))
+    var FJP = Formacao("Formacao Java-Python", mutableSetOf(CE2, CE3, CE4))
+    
+    FJK.matricular(user1);
+    FJK.matricular(user3);
+    
+    println(FJK.inscritos);
+    
+    FJK.cancelarMatricula(user1.email);
+    
+    println(FJK.inscritos);
+    
+//     println(FJK.toString());
+//     println(FJK.conteudos);
+//     println(FJK.inscritos);
+    
 }
+
+
+
+// A DIO possui Formacoes incríveis que têm como objetivo oferecer um conjunto de ConteudosEducacionais voltados para uma stack tecnológica específica,
+// preparando profissionais de TI para o mercado de trabalho. Formacoes possuem algumas características importantes, como nome, nivel e seus respectivos
+// conteudosEducacionais. Além disso, tais experiências educacionais têm um comportamento relevante ao nosso domínio, definido pela capacidade de
+// matricular um ou mais Alunos.
